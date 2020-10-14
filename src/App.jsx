825a0eeacb53b35d17710580ee65facf568dd98f
ikdshn <harohawayu0504@gmail.com>
2020-10-14 20:20:57 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Room from "./pages/Room";
+
+const App = () => {
+  return (
+    <>
+      <Login />
+      <Signup />
+      <Room />
+    </>
+  );
+};
+
+export default App;
