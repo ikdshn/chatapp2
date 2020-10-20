20fda42d48ab250eafb43676b025ddbf1bf37fa6
ikdshn <harohawayu0504@gmail.com>
2020-10-21 00:43:16 +0900

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Room from "./pages/Room";
 
+// routing (react-router-dom)
+// URLのエンドイポイント(/xxx)によって表示ページを変える
+
 const App = () => {
   return (
     <>
